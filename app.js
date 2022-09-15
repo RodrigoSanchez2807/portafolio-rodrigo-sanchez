017a72157a907738cf3a1c7908eb43d82ef20261
RodrigoSanchez2807 <rodalbertlopez@gmail.com>
2022-09-15 16:45:41 -0500

    menu

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const toggleMenu = document.querySelector('#toggle');
 const navigation = document.querySelector('#navigation');
+const menuItem = document.querySelectorAll('.menu-item');
+const iconItem = document.querySelectorAll('.menu-item-icon');
 
-toggleMenu.addEventListener('click', showMenu);
+toggleMenu.addEventListener('click', () => {navigation.classList.toggle('navigation--show');});
 
-function showMenu () {
-    navigation.classList.toggle('navigation--show');
-    toggleMenu.classList.toggle('.menu-tog--close::before');
+
+
+function hideMenu(){
+    navigation.classList.remove('navigation--show');
 }
+menuItem.forEach(n => n.addEventListener('click', hideMenu));
+
+iconItem.forEach(n => n.addEventListener('click', hideMenu));
